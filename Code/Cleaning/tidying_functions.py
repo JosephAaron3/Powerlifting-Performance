5f959fca6d8e5709f56799604015d67d4bf2e837
Joseph Menesch <joe.menesch@gmail.com>
2021-03-31 22:30:38 +1000

    All columns are variables

--- Code/Cleaning/tidying_functions.py
+++ Code/Cleaning/tidying_functions.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 import math
-
-df = pd.read_csv("../../Data/Raw_Full.csv", engine = 'python')
-
+import seaborn as sb
+import matplotlib.pyplot as plt
 
 def separate_links(df): # Separate comp link to remove redundency
      return df[["CompID", "Link"]].drop_duplicates(), df.drop(columns = ['Link'])
@@ -19,7 +18,7 @@ def check_for_substrings(ls, df):
                                                 na = False)]['CompID'].values))
     return sorted(set(Comps))
     
-def remove_bad_rowscols(dfd):
+def remove_bad_rowscolstables(dfd):
     for i, cdf in dfd.items():
         dfd[i] = cdf.dropna(axis = 1, how = 'all')
         for j, k in cdf.iterrows():
@@ -30,6 +29,7 @@ def remove_bad_rowscols(dfd):
         del dfd[key]
     
 def find_header_exceptions(dfd):
+    #Simply find dfs without "Name" in the top row - Naive method, but fine for this dataset
     substrings = ['NAME', 'Name']
     ls = []
     for i, cdf in dfd.items():
@@ -41,13 +41,15 @@ def find_header_exceptions(dfd):
     return ls
 
 def fix_headers_custom(bad_heads, dfd):
-    #Lingering superfluous top row - 24, 74, 75, 95, 97, 177, 195, 463(x2), 
-    #nan where name should be - 36, 187, 214(and sex)
-    #Separate rows for lifts and attempt# - 45, 61, 72, 166
-    #Name column called something else (e.g. "Lifter") - 59, 107, 129, 201
-    #Header row completely missing - 706
-    #Strange format - 829
-    #CompID is just the compId number - All
+# =============================================================================
+# Lingering superfluous top row - 24, 74, 75, 95, 97, 177, 195, 463(x2), 
+# nan where name should be - 36, 187, 214(and sex)
+# Separate rows for lifts and attempt# - 45, 61, 72, 166
+# Name column called something else (e.g. "Lifter") - 59, 107, 129, 201
+# Header row completely missing - 706
+# Strange format - 829
+# CompID is just the compId number - All
+# =============================================================================
     for compid in bad_heads:
         if compid in [24, 74, 75, 95, 97, 177, 195, 463]:
             dfd[compid] = dfd[compid].iloc[1:]
@@ -72,16 +74,18 @@ def fix_headers_custom(bad_heads, dfd):
         elif compid in [829]:
             del dfd[compid]
             
-    #Fix CompID column
+    #Fix '.' columns
     for i, cdf in dfd.items():
-        dfd[i] = dfd[i].astype(str)
-        dfd[i].iat[0, 0] = "CompID"
+        dfd[i].iat[0,0] = -1 #For easy referencing later
         if dfd[i].iat[0, 1] == '.':
             dfd[i].iat[0, 1] = "Place"
     
 def promote_headers(dfd):
     for i, cdf in dfd.items():
         dfd[i].columns = cdf.iloc[0]
+        if (len(dfd[i][-1].shape) != 1):
+            print("Error: Multiple columns named -1")
+        dfd[i].rename(columns = {-1: "CompID"}, inplace = True)
         dfd[i] = dfd[i].iloc[1:]
 
 def col_name_count(dfd, names):
@@ -96,23 +100,53 @@ def col_name_count(dfd, names):
         name_freq[name] = (count, ls)
     return name_freq
 
-# def test(dfd, words):
-#     Main = {}
-#     for main in words.columns.to_list():
-#         name_freq = {}
-#         for name in words[main].to_list():
-#             if type(name) == float and math.isnan(name):
-#                 continue
-#             count = 0
-#             ls = []
-#             for k, cdf in dfd.items():
-#                 if name.strip()[1:-1] in cdf.columns.to_list():
-#                     count += 1
-#                     ls.append(k)
-#             name_freq[name] = (count, ls)
-#         Main[main] = name_freq
-#     return Main
+def na_col_details(dfd):
+    #Return the dfs that have nulls, and where those nulls are, and plot the distribution
+    na_ls = [k for k, cdf in dfd.items() if cdf.columns.hasnans]
+    na_dict = {k: [i for i, isnan in enumerate(cdf.columns.isna()) if isnan] for k, cdf in dfdict.items() if k in na_ls}
+    print(len(dfdict) - len(na_ls), "don't have na columns")
+    print(len(na_ls), "have na columns")
+    sb.set_style("whitegrid")
+    plt.figure(figsize = (10,5))
+    sb.distplot(na_ls, bins = 20, kde = True, color = 'teal', kde_kws=dict(linewidth = 4, color = 'black'))
+    plt.show()
+    return na_dict
     
+def handle_na_cols_custom(dfd):
+    manual_cases = [(45, 'Best', 'Delete'),
+    (61, 'Best', 'Delete', 'Delete'),
+    (64, 'Delete'),
+    (72, 'TOTAL', 'Delete', 'Delete', 'WILKS'),
+    (103, 'Squat', 'Delete', 'Deadlift', 'Delete'),
+    (110, 'Delete', 'Delete'),
+    (114, 'Squat', 'Delete', 'Deadlift', 'Delete'),
+    (117, 'Squat', 'Squat', 'Bench', 'Bench', 'Deadlift', 'Deadlift', 'Delete', 'Delete', 'Delete', 'Delete'),
+    (140, 'Delete'),
+    (147, 'Squat', 'Squat', 'Delete', 'Bench', 'Bench', 'Delete', 'Deadlift', 'Deadlift', 'Delete'),
+    (166, 'Best'),
+    (411, 'Delete', 'Delete')]
+    
+    for case in manual_cases:
+        case_no = case[0]
+        s = pd.Series(dfd[case_no].columns)
+        s = s.fillna('ToBeChanged' + (s.groupby(s.isnull()).cumcount()).astype(str))
+        dfd[case_no].columns = s #Change nan column names to iterable names
+        for num, col in enumerate(case[1:]):
+            if col == 'Delete':
+                dfd[case_no].drop(columns = {'ToBeChanged'+str(num)}, inplace = True)
+            else:
+                dfd[case_no].rename(columns = {'ToBeChanged'+str(num): col}, inplace = True)
+
+def remove_na_col1(dfd, na_dict):
+    for k, ls in na_dict.items():
+        if 1 in ls:
+            keep_cols = list(range(len(dfd[k].columns)))
+            keep_cols.remove(1)
+            dfd[k] = dfd[k].iloc[:, keep_cols]
+    
+def remove_na_col_rest(dfd, na_dict):
+    for k in na_dict.keys():
+        dfd[k] = dfd[k].loc[:, dfd[k].columns.notnull()]
 
 if __name__ == "__main__":
     #Read data
@@ -128,12 +162,12 @@ if __name__ == "__main__":
     #Split df into dictionary of dfs for each competition
     dfdict = full_split(df)
     
-    #Find comps with equipped lifters
+    #Find comps with equipped lifters (for potential use later)
     Eqp_Words = ["EQD", "EQP", "Equip", "EQUP"]
     Equipped_Comps = check_for_substrings(Eqp_Words, df)
     
-    #Remove blank columns and superfluous rows, and deletes blank dataframes
-    remove_bad_rowscols(dfdict)
+    #Remove blank columns and unnecessary rows, and delete blank dataframes
+    remove_bad_rowscolstables(dfdict)
     
     #Now first row should be header values. We'll check for exceptions manually
     bad_header_dfs = find_header_exceptions(dfdict)
@@ -144,22 +178,70 @@ if __name__ == "__main__":
     
     #Check fix worked
     bad_header_dfs = find_header_exceptions(dfdict)
-    print("Broken headers: ", bad_header_dfs)
+    print("Broken headers after fix: ", bad_header_dfs)
     
     #Promote headers
     promote_headers(dfdict)
     
-    #Check the full list of headers in each row
+    #Remove bad rows/cols again (some could have emerged from previous operations)
+    remove_bad_rowscolstables(dfdict)
+    
+    #Check the full list of headers from each df
     col_names = []
     for k, cdf in dfdict.items():
         col_names.extend(cdf.columns.to_list())
-    print("Headers set: ", sorted(set(col_names))) #Sorted these headers into groups manually (Semantics.csv)
+    print("Headers set: ", set(col_names)) #Sorted these headers into groups manually (See Semantics.csv)
     
-    #Found count of instances of each of these
+    #Find count of instances of each of these
     header_name_freq = col_name_count(dfdict, col_names)
     # sem = pd.read_csv("../../Data/Semantics.csv", )
     # testdf = test(dfdict, sem)
-    
     #By inspection, these mostly appear to be variables, apart from nan, which requires further investigation
+    
+    #Check where na columns are located
+    na_dict = na_col_details(dfdict)
+    #Also, most na cols are in the latter half of the comps
+    
+    #Handle the na columns by either removing or renaming. Note that 505/517 (Comp 454+) are of very similar format,
+    # so I'll deal with the first 12 individually then handle the others together
+    handle_na_cols_custom(dfdict) #NOTE: Data-specific implementation
+    
+    #Recheck na columns
+    na_dict = na_col_details(dfdict)
+    #Most of these only have NA in column 1, which is place column. Let's verify this
+    
+    vals_in_col1 = {k: list(dfdict[k].iloc[:, [1]].values.flatten()) for k, ls in na_dict.items() if 1 in ls}
+    #By inspection, all column 1 na columns are placings and can be removed
+    
+    #Remove na columns occuring in column 1
+    remove_na_col1(dfdict, na_dict)
+    
+    #Recheck na columns
+    na_dict = na_col_details(dfdict)
+    #Down from 505 tables with na columns to 174 tables.879
+    #By inspection (of [[print(list(dfdict[k].iloc[:, num].values))) for num in cols] for k, cols in na_dict.items())]),
+    # all other na columns are mostly na, containing only Australian/world record info. The exception to this is the nan
+    # in comp 879, which is just a duplicate column anyway
+    
+    #Remove all remaining na columns
+    remove_na_col_rest(dfdict, na_dict)
+    
+    #Recheck na columns
+    na_dict = na_col_details(dfdict)
+    #Now all columns are variables
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
